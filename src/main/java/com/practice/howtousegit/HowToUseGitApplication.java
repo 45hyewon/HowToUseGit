7dc5c93bf474ec40a349ce80755e554d7957642a
45hyewon <haewon0611@gmail.com>
2023-08-07 19:08:18 +0900

    1

---
+++ src/main/java/com/practice/howtousegit/HowToUseGitApplication.java
@@ -0,0 +1,13 @@
+package com.practice.howtousegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToUseGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowToUseGitApplication.class, args);
+    }
+
+}
